717636a5e9753f3683071957e649d21a64cf776d
Sitthata <aprils96950@gmail.com>
2023-10-17 22:33:20 +0700

    Hotfix on event

--- src/components/Events/TopLine.tsx
+++ src/components/Events/TopLine.tsx
@@ -1,7 +1,7 @@
 const TopLine = () => {
   return (
     <img
-      className="min-w-screen absolute top-0  z-[-1] min-h-[25%]"
+      className="min-w-screen absolute top-0 min-h-[25%]"
       src="/images/events/top.png"
       alt="top"
     />
--- src/components/Team/TeamItem.tsx
+++ src/components/Team/TeamItem.tsx
@@ -16,25 +16,26 @@ const TeamItem = ({ team, isPreview = false }: TeamItemProps) => {
     },
   };
   return (
-    <motion.div
-      className={twMerge(
-        "relative flex aspect-square cursor-pointer flex-col items-center justify-end rounded-2xl py-4 shadow-lg transition-all duration-300",
-        isPreview ? "bg-[#0F0A4CCC]" : "bg-[#6479BCE5]",
-      )}
-      variants={buttonList}
-      whileHover="hover"
-      whileTap="tap"
-      transition={{ duration: 0.2 }}
-    >
-      <img
-        src={team.image}
-        alt={team.name}
-        className="absolute bottom-[35%] h-full w-full max-w-none transform drop-shadow-lg filter lg:max-w-[80rem]"
-      />
-      <h3 className="z-10 whitespace-nowrap text-[0.85rem] font-bold text-white sm:text-[1.7rem] lg:text-[2.15rem]">
-        {team.name}
-      </h3>
-    </motion.div>
+      <motion.div
+        className={twMerge(
+          "group relative max-w-[20rem] flex aspect-square cursor-pointer flex-col items-center justify-end rounded-2xl py-4 shadow-lg transition-all duration-300",
+          isPreview ? "bg-[#0F0A4CCC]" : "bg-[#6479BCE5]",
+        )}
+        variants={buttonList}
+        whileHover="hover"
+        whileTap="tap"
+        transition={{ duration: 0.2 }}
+        layout
+      >
+        <img
+          src={team.image}
+          alt={team.name}
+          className="z-20 group-hover:translate-y-[-1.5rem] transition-all duration-500 absolute bottom-[20%] h-full w-full max-w-none transform drop-shadow-lg filter lg:max-w-[80rem]"
+        />
+        <h3 className="z-10 whitespace-nowrap text-[0.55rem] font-bold text-white sm:text-[1.7rem] lg:text-[2.15rem]">
+          {team.name}
+        </h3>
+      </motion.div>
   );
 };
 
