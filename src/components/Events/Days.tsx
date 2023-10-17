6facd19aba11f6bc8f93ba95ffc987a805cdfd83
realFK98 <wishira2013.coth@gmail.com>
2023-10-17 21:02:22 +0700

    add animation event

--- src/components/Events/Days.tsx
+++ src/components/Events/Days.tsx
@@ -1,17 +1,33 @@
-import { HTMLMotionProps } from "framer-motion";
+import { HTMLMotionProps, motion } from "framer-motion";
 import React from "react";
 import { twMerge } from "tailwind-merge";
 
 type DaysProps = {
   day: string;
+  duration?: number;
+  delay?: number;
 } & HTMLMotionProps<"img">;
 
-const Days: React.FC<DaysProps> = ({ day, className }) => {
+const Days: React.FC<DaysProps> = ({
+  day,
+  className,
+  duration = 3,
+  delay = 0,
+}) => {
+  const dayAnimation = {
+    day: {
+      scale: [1, 1.05, 1],
+      transition: { duration: duration, repeat: Infinity, delay: delay },
+    },
+  };
   return (
-    <img
+    <motion.img
       className={twMerge("absolute w-[45%]", className)}
       src={`/images/events/${day}.png`}
       alt={`${day}`}
+      variants={dayAnimation}
+      initial="day"
+      animate="day"
     />
   );
 };
--- src/components/Events/EventsDesktop.tsx
+++ src/components/Events/EventsDesktop.tsx
@@ -4,22 +4,22 @@ import GenerateFlower from "./GenerateFlower";
 const EventsDesktop = () => {
   const flowers = [
     { position: "left-[-10%] top-[16%] w-[15%]", colorFlower: "pink" },
-    { position: "right-[1%] top-[5%] w-[14%]", colorFlower: "orange" },
-    { position: "bottom-[32%] right-[2%] w-[16%]", colorFlower: "pink" },
-    { position: "bottom-[33%] left-[-5%] w-[16%]", colorFlower: "purple" },
+    { position: "right-[-20%] top-[5%] w-[14%]", colorFlower: "orange" },
+    { position: "bottom-[32%] right-[-20%] w-[16%]", colorFlower: "pink" },
+    { position: "bottom-[33%] left-[-32%] w-[17%]", colorFlower: "purple" },
   ];
   const days = [
-    { position: "bottom-[2%] right-[-12%]", day: "Graduation" },
-    { position: "bottom-[45%] right-[-15%] ", day: "gooseDay" },
-    { position: "left-[5%] top-[5%]", day: "registerDay" },
-    { position: "bottom-[8%] left-[-5%]", day: "finalDay" },
+    { position: "bottom-[2%] right-[-12%]", day: "Graduation", delay: 1 },
+    { position: "bottom-[45%] right-[-15%] ", day: "gooseDay", delay: 3 },
+    { position: "left-[5%] top-[5%]", day: "registerDay", delay: 5 },
+    { position: "bottom-[8%] left-[-5%]", day: "finalDay", delay: 4 },
   ];
   return (
-    <div className="relative my-auto hidden scale-[75%] items-center justify-center md:block lg:top-[-20px] TabletV:hidden">
+    <div className="relative my-auto hidden scale-[80%] items-center justify-center md:block lg:top-[-20px] TabletV:hidden">
       <img className="mx-auto " src="/images/events/platform.png" alt="" />
       <GenerateFlower flowers={flowers} />
-      {days.map(({ position, day }) => (
-        <Days day={`${day}`} className={`${position}`} />
+      {days.map(({ position, day, delay }) => (
+        <Days day={`${day}`} className={`${position}`} delay={delay} />
       ))}
     </div>
   );
