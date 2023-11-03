bb17c3ae47287682cbc56a7775d72ca024d1f3d4
realFK98 <wishira2013.coth@gmail.com>
2023-11-03 23:30:16 +0700

    fix link social

--- src/components/Social/SocialLink.tsx
+++ src/components/Social/SocialLink.tsx
@@ -8,6 +8,7 @@ type SocialLinkProps = {
 
 const SocialLink = ({ href, icon, className, ...props }: SocialLinkProps) => {
   return (
+<<<<<<< Updated upstream
     <button
       className={twMerge(
         "flex sm:h-[4rem] sm:w-[4rem] w-[3rem] h-[3rem] cursor-pointer items-center justify-center rounded-full bg-white text-black hover:bg-opacity-80 transition duration-300",
@@ -15,14 +16,25 @@ const SocialLink = ({ href, icon, className, ...props }: SocialLinkProps) => {
       )}
       {...props}
     >
+=======
+    <>
+>>>>>>> Stashed changes
       {href && (
         <a href={href} target="_blank" rel="noreferrer">
-          {icon}
+          <button
+            className={twMerge(
+              "flex h-[3rem] w-[3rem] cursor-pointer items-center justify-center rounded-full bg-pink-300 text-white transition duration-300 hover:bg-opacity-80 sm:h-[4rem] sm:w-[4rem]",
+              className,
+            )}
+            {...props}
+          >
+            {icon}
+
+            {!href && icon}
+          </button>
         </a>
       )}
-
-      {!href && icon}
-    </button>
+    </>
   );
 };
 
