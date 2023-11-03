112c33403c08c9be01a993471ec2e083f5af2ab7
realFK98 <wishira2013.coth@gmail.com>
2023-11-03 23:47:57 +0700

    fix link social

--- src/components/Social/SocialLink.tsx
+++ src/components/Social/SocialLink.tsx
@@ -8,17 +8,7 @@ type SocialLinkProps = {
 
 const SocialLink = ({ href, icon, className, ...props }: SocialLinkProps) => {
   return (
-<<<<<<< Updated upstream
-    <button
-      className={twMerge(
-        "flex sm:h-[4rem] sm:w-[4rem] w-[3rem] h-[3rem] cursor-pointer items-center justify-center rounded-full bg-white text-black hover:bg-opacity-80 transition duration-300",
-        className,
-      )}
-      {...props}
-    >
-=======
     <>
->>>>>>> Stashed changes
       {href && (
         <a href={href} target="_blank" rel="noreferrer">
           <button
